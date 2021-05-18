e8ddd9182667f4a2b541ca66180734f7b1a8aa2e
brodyck <brody.m.dyck@tutanota.com>
2021-05-18 02:11:55 -0400

    working on making the download of the ISO work. I had it download once but it hasn't done anything with the ISO yet.

--- cfg/virtualisation/vms/pfsense.nix
+++ cfg/virtualisation/vms/pfsense.nix
@@ -1,75 +1,76 @@
-{ config, lib, pkgs, ... }:
-# SEO:
-# bsd freebsd ports nixos declaritive automated edge router edge-router
-# 
-# somewhat ripped from the wonderful kalbasit
-# https://github.com/kalbasit/soxincfg/tree/main/hosts/hades
-# as I have no idea what I'm doing.
-#
-# Goal sumary is to have it create a pfsense VM that handles WAN + LAN (plus VPN) traffic.
-# Steps:
-# 0. learn nix stuff lol
-# 1. download pfsense
-# 2. check it against the provided sha256 file
-# 3. create a raw image of specified size inside specified directory
-# 4. create libvirt xml conf
-# 5. Use a direct console (not IP) to run commands:
-#   - install Nix from ports
-#   - not sure of the other commands needed just yet
-#     - have to figure out what exactly can be done from CLI in pfsense, and how
-# 6. isolate cpu cores for network performance
-# 7. create BSD based nix binary cache
-# 8. create a way on the host that I can deploy nix to pfsense
-#
-#
-
+{ config, lib, pkgs, unzip, ... }:
 with lib;
 let
   version = "2.5.1";
-  buildPfsense = vmName:
-    {
+  # ya I don't know if this works yet honestly
 
-      # ya I don't know if this works yet honestly
-      iso = pkgs.fetchurl {
-        url = https://nyifiles.netgate.com/mirror/downloads/pfSense-CE-${version}-RELEASE-amd64.iso.gz;
-	sha256 = sha256sum;
+#  download = {
+#    pfsense = rec {
+#      iso = builtins.fetchurl {
+#        url = "https://nyifiles.netgate.com/mirror/downloads/pfSense-CE-${version}-RELEASE-amd64.iso.gz";
+#        sha256 = "be79df534558e6a73f7be2e8643c6ed01580e40b79b255f9bd8e8cca6471fee7";
+#      };
+#    };
+#  };
+
+  # what i have here is based on what I found here:
+  # http://johnmercier.com/blog/2017/12-28-adding-jbake-to-nixpkgs.html
+  # which has a section that makes more sense than anything in the docs.
+  download = {stdenv, fetchurl, unzip}:
+    pkgs.stdenv.mkDerivation {
+      name = "pfsense-${version}";
+      src = builtins.fetchurl {
+        url = "https://nyifiles.netgate.com/mirror/downloads/pfSense-CE-${version}-RELEASE-amd64.iso.gz";
+        sha256 = "be79df534558e6a73f7be2e8643c6ed01580e40b79b255f9bd8e8cca6471fee7";
       };
+      buildInputs = [pkgs.unzip];
+      phases = [ "unpackPhase" "installPhase" ];
+      unpackPhase = ''
+        ls -lah
+        unzip ${src}  
+        ls -lah           
+      '';  
+      installPhase = ''
+        mv ${name} $out/storage/vms/pfsense/
+      '';          
+    };
 
-      sha256sum = pkgs.fetchurl {
-        url = https://www.pfsense.org/hashes/pfSense-CE-${version}-RELEASE-amd64.iso.gz.sha256;
-      };
 
-      after = [ "libvirtd.service" ];
-      requires = [ "libvirtd.service" ];
-      serviceConfig = {
-        Type = "oneshot";
-        RemainAfterExit = "yes";
-      };
-      restartIfChanged = true;
+  
+  buildPfsense = vmName: {
+    
+    after = [ "libvirtd.service" ];
+    requires = [ "libvirtd.service" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = "yes";
+    };
+    restartIfChanged = true;
 
-      script =
-        let
-          xml = pkgs.substituteAll {
-            src = ./pfsense.xml;
+    script =
+      let
+        xml = pkgs.substituteAll {
+          src = ./pfsense.xml;
 
-            name = vmName;
-            ovmf_q35 = "pc-q35-5.1";
-            net_lan_source_dev = "pfsense-lan";
-            net_lan_mac_address = "68:05:c4:20:69:21";
-            net_wan_source_dev = "pfsense-wan";
-            net_wan_mac_address = "68:05:c4:20:69:20";
-            disk_img = "/storage/vm/pfsense/pfsense.raw";
-            disk_iso = "/storage/vm/pfsense/pfSense-CE-${version}-RELEASE-amd64.iso";
-          };
+          name = vmName;
+          ovmf_q35 = "pc-q35-5.1";
+          net_lan_source_dev = "pfsense-lan";
+          net_lan_mac_address = "68:05:c4:20:69:21";
+          net_wan_source_dev = "pfsense-wan";
+          net_wan_mac_address = "68:05:c4:20:69:20";
+          disk_img = "/storage/vm/pfsense/pfsense.raw";
+          disk_iso = "/storage/vm/pfsense/pfSense-CE-${version}-RELEASE-amd64.iso";
+#          disk_iso = "${download}";
+        };
 
-        in
+      in
         ''
           uuid="$(${getBin pkgs.libvirt}/bin/virsh domuuid '${vmName}' || true)"
           ${getBin pkgs.libvirt}/bin/virsh define <(sed "s/UUID/$uuid/" '${xml}')
           ${getBin pkgs.libvirt}/bin/virsh start '${vmName}'
         '';
 
-      preStop = ''
+    preStop = ''
         ${getBin pkgs.libvirt}/bin/virsh shutdown '${vmName}'
         let "timeout = $(date +%s) + 120"
         while [ "$(${getBin pkgs.libvirt}/bin/virsh list --name | grep --count '^${vmName}$')" -gt 0 ]; do
@@ -81,10 +82,13 @@ let
             sleep 0.5
           fi
         done
-      '';
-    };
+    '';
+  };
+  #getIso = callPackage download {};
 in
 {
+  callPackage = download {};
   systemd.services.libvirtd-guest-pfsense = buildPfsense "pfsense";
 }
+    
 
